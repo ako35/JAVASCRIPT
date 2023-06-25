4a0394d15fb3d3d6319ae49a3e419480ab344a92
Ali KOÇAN <alikcn35@gmail.com>
2023-06-25 07:38:56 +0300

    javascriptday16

---
+++ assets/js/87-script.js
@@ -0,0 +1,22 @@
+const hourEl=document.querySelector('#clock li:nth-child(1)');
+const secondEl=document.querySelector('#clock li:nth-child(2)');
+const minuteEl=document.querySelector('#clock li:nth-child(3)');
+
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+hourEl.innerHTML = hours.length===1 ? `0${hours}` : hours;
+minuteEl.innerHTML = minutes.length===1 ? `0${minutes}` : minutes;
+secondEl.classList.toggle('hidden');
+
+setInterval(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    hourEl.innerHTML = hours.length===1 ? `0${hours}` : hours;
+    minuteEl.innerHTML = minutes.length===1 ? `0${minutes}` : minutes;
+    secondEl.classList.toggle('hidden');
+},1000);
+
